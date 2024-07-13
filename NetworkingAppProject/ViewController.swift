b7924948183ca72cb9623d0c2b2b584c9d544941
LalaAghyva <laleagayeva78@gmail.com>
2024-07-13 20:03:10 +0400

    Initial Commit

---
+++ NetworkingAppProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingAppProject
+//
+//  Created by Lala on 13.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
